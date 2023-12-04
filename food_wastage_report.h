ed70545e421907e5bf0b9ff5de200a3da73d0796
bryqnt <123854285+bryqnt@users.noreply.github.com>
2023-12-04 14:38:11 -0800

    Create food_wastage_report.h

---
+++ food_wastage_report.h
@@ -0,0 +1,22 @@
+#ifndef FOOD_WASTAGE_REPORT_H_
+#define FOOD_WASTAGE_REPORT_H_
+
+// Bryant Huynh
+// CPSC 121L-01
+// 11/27/2023
+// 22bhuynh2@csu.fullerton.edu
+// @bryqnt
+
+class FoodWastageReport {
+  // ======================= YOUR CODE HERE =======================
+  // Write the FoodWastageReport class here. Refer to your class design for the
+  // member variables, constructors, and member functions needed.
+  //
+  // Note: mark functions that do not modify the member variables
+  // as const, by writing `const` after the parameter list.
+  // Pass objects by const reference when appropriate.
+  // Remember that std::string is an object!
+  // ===============================================================
+};
+
+#endif
