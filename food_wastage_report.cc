fc13b6e0bb05385e8de72e2455543b0a3e8ecb84
bryqnt <123854285+bryqnt@users.noreply.github.com>
2023-12-04 14:37:54 -0800

    Create food_wastage_report.cc

---
+++ food_wastage_report.cc
@@ -0,0 +1,20 @@
+// Bryant Huynh
+// CPSC 121L-01
+// 11/27/2023
+// 22bhuynh2@csu.fullerton.edu
+// @bryqnt
+
+#include "food_wastage_report.h"
+
+// ========================= YOUR CODE HERE =========================
+// This implementation file is where you should implement
+// the member functions declared in the header (food_wastage_report.h), only
+// if you didn't implement them inline within food_wastage_report.h.
+//
+// Remember to specify the name of the class with :: in this format:
+//     <return type> MyClassName::MyFunction() {
+//        ...
+//     }
+// to tell the compiler that each function belongs to the FoodWastageReport
+// class.
+// ===================================================================
